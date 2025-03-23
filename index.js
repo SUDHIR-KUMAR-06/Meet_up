7ecc52366ccc940c815e247f353277648f696355
SUDHIR KUMAR <snghsudhirkumar06@gmail.com>
2025-03-23 23:55:26 +0530

    till css setup

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const io = require("socket.io")(server);
 
 app.use(express.static("public"));
 
+let connectedPeers = [];
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/piblic/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  connectedPeers.push(socket.id);
+  //   console.log(connectedPeers);
+
+  socket.on("disconnect", () => {
+    // console.log("user disconnected");
+    const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
+      peerSocketId !== socket.id;
+    });
+    connectedPeers = newConnectedPeers;
+    // console.log(connectedPeers);
+  });
 });
 
 server.listen(PORT, () => {
