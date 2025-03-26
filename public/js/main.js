770d22874ecc32b2ac7149b2dbcf478cfabbbce1
SUDHIR KUMAR <snghsudhirkumar06@gmail.com>
2025-03-26 14:30:45 +0530

    state_management

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
+import * as store from "./store.js";
+
 const socket = io("/");
 
 socket.on("connect", () => {
   console.log("successfully connected");
-  console.log(socket.id);
+  store.setSocketId(socket.id);
 });
